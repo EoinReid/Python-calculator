2f959f496b5d62daae1b70ce2460d00ff0668924
Eoin Reid <40269943+EoinReid@users.noreply.github.com>
2022-10-21 11:21:25 +0100

    add calculation functions & improvements
    Created functions for each calculation type and sarted calling them from the if statements instead of doing the calculations there directly.
    Also convert the number input from string to float.

--- calculator.py
+++ calculator.py
@@ -1,25 +1,39 @@
 # user interface
-number_one = input("Enter a number: ")
-number_two = input("Enter another number: ")
+# convert user input (which defaults as string, to float)
+number_one = float(input("Enter a number: "))
+number_two = float(input("Enter another number: "))
+operation = input("Choose an operation: + - * /")
+
+# functions for calculations
+def add(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1*num2
+
+def divide(num1, num2):
+    return num1/num2
 
 #calculations
-operation = input("Choose an operation: + - * /")
 # if user enters +, do addition
-# making sure to convert number_one and number_two to int, as the variables are a str due to taking user input.
-# e.g without converting to int if user enters 5 and 10 for number_one and number_two and chooses + the result will be 510, instead of 15.
 if operation == "+":
-    print(int(number_one) + int(number_two))
+    # calls add() method to do calculations instead of doing it within the if statement
+    # No longer converting type in if statement but at input.
+    print(add(number_one, number_two))
 # else if user enters -, do subtraction
-# using max() and min() to prevent a negative number and ensuring the bigger number is always subtracted by the smallerst.
 elif operation == "-":
-    print(max(int(number_one), int(number_two)) - min(int(number_one), int(number_two)))
+    # calls minus() method to do calucation
+    print(minus(number_one, number_two))
 # else if user enters *, do multiplication
 # converting to integer similar to addition operation
 elif operation == "*":
-    print(int(number_one)*int(number_two))
+    print(multiply(number_one, number_two))
 # else if user enters /, do division
 elif operation == "/":
-    print(max(number_one, number_two)/min(number_one, number_two))
+    print(divide(number_one, number_two))
 # else if the user enters anything other than the above 4 operators, display a warning that they are not entering a valid operator.
 else:
     print("Not a valid operator, please try again.")
